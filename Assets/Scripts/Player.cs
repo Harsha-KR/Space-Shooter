50940efa51f2ec08c4069e3f57a668c953242992
Harshak-r1 <Harsha-kr1@users.noreply.github.com>
2021-05-24 18:27:28 +0530

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+    }
+
+    private void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3f, 0f), 0f);
+        
+        if (transform.position.x > 9.3f)
+        {
+            transform.position = new Vector3(-9.3f, transform.position.y, 0f);
+        }
+        else if (transform.position.x < -9.3f)
+        {
+            transform.position = new Vector3(9.3f, transform.position.y, 0f);
+        }
+    }
+}
